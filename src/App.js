90fe50821d5e3eb0519aebe57d5d46937cc82679
Adil Islambekov <osnapitzadi@gmail.com>
2021-02-01 14:32:08 -0500

    auth-done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react'
+import SignUp from './components/SignUp';
+import { Container } from 'react-bootstrap';
+import { AuthProvider } from './contexts/AuthContext.js';
 import './App.css';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from './components/ForgotPassword';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container  
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}>
+        <div 
+          className="w-100"
+          style={{maxWidth: "400px"}}>
+            <Router>
+              <AuthProvider>
+                <Switch>
+                  <PrivateRoute exact path='/' component={Dashboard} />
+                  <Route path='/signup' component={SignUp} />
+                  <Route path='/login' component={Login} />
+                  <Route path='/forgot-password' component={ForgotPassword} />
+                </Switch>
+              </AuthProvider>
+            </Router>
+        </div>
+    </Container>
   );
 }
 
